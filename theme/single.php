c72888c718d4f4405c5920f10053d54decb976cb
Matt Sloan <msloan@knotice.com>
2016-07-24 15:10:19 -0400

    added custom meta box with image upload (not working)

--- theme/single.php
+++ theme/single.php
@@ -6,5 +6,6 @@
     <?php the_post(); ?>
     <?php the_content(); ?>
     <?php edit_post_link( __( 'Edit', 'TB2017' ), '<span class="edit-link">', '</span>' ) ?>
+    <?php echo get_post_meta(get_the_ID(), 'vendor_img_widget_1', true); ?>
 </div>
 <?php get_footer(); ?>
