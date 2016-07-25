e011f6617aa22ef3649442bf4f8503a000c5b553
Matt Sloan <msloan@knotice.com>
2016-07-24 22:59:52 -0400

    added working meta box with image upload.

--- plugins/vendors/vendors.php
+++ plugins/vendors/vendors.php
@@ -63,89 +63,196 @@ function vendor_listings() {
 add_action( 'init', 'vendor_listings' );
 
 
-function add_vendor_meta_boxes() {
-
-  function vendor_img_widget_1() {
-
-    wp_nonce_field(plugin_basename(__FILE__), 'vendor_img_widget_1_nonce');
-     
-    $html = '<p class="description">';
-        $html .= 'Upload an image. Supported file types are jpg/jpeg, png, or gif.';
-    $html .= '</p>';
-    $html .= '<input type="file" id="vendor_img_widget_1" name="vendor_img_widget_1" value="" size="25" />';
-     
-    echo $html;
-
-  }
 
 
 
-  function save_custom_meta_data($id) {
-   
-    /* --- security verification --- */
-    if(!wp_verify_nonce($_POST['vendor_img_widget_1_nonce'], plugin_basename(__FILE__))) {
-      return $id;
-    } // end if
-       
-    if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-      return $id;
-    } // end if
-       
-    if('page' == $_POST['post_type']) {
-      if(!current_user_can('edit_page', $id)) {
-        return $id;
-      } // end if
-    } else {
-        if(!current_user_can('edit_page', $id)) {
-            return $id;
-        } // end if
-    } // end if
-    /* - end security verification - */
-       
-    // Make sure the file array isn't empty
-    if(!empty($_FILES['vendor_img_widget_1']['name'])) {
-         
-      // Setup the array of supported file types. In this case, it's just PDF.
-      $supported_types = array('image/jpeg', 'image/png', 'image/gif');
-       
-      // Get the file type of the upload
-      $arr_file_type = wp_check_filetype(basename($_FILES['vendor_img_widget_1']['name']));
-      $uploaded_type = $arr_file_type['type'];
-       
-      // Check if the type is supported. If not, throw an error.
-      if(in_array($uploaded_type, $supported_types)) {
 
-          // Use the WordPress API to upload the file
-          $upload = wp_handle_upload($_FILES['vendor_img_widget_1']['name'], null, file_get_contents($_FILES['vendor_img_widget_1']['tmp_name']));
-   
-          if(isset($upload['error']) && $upload['error'] != 0) {
-              wp_die('There was an error uploading your file. The error is: ' . $upload['error']);
-          } else {
-              add_post_meta($id, 'vendor_img_widget_1', $upload);
-              update_post_meta($id, 'vendor_img_widget_1', $upload);     
-          } // end if/else
 
-      } else {
-          wp_die("The file type that you've uploaded is not supported.");
-      } // end if/else
-         
-    } // end if
-  } // end save_custom_meta_data
-  add_action('save_post', 'save_custom_meta_data');
 
-  // function update_edit_form() {
-  //   echo ' enctype="multipart/form-data"';
-  // } // end update_edit_form
-  // add_action('post_edit_form_tag', 'update_edit_form');
 
-  // Define the custom attachment for vendor post type
-  add_meta_box(
-    'vendor_img_widget_1', //id
-    'Vendor Image Widget', // label
-    'vendor_img_widget_1', // callback
-    'vendor', // post type
-    'normal' // location
-  );
-}
-add_action('add_meta_boxes', 'add_vendor_meta_boxes');
+
+              /**
+               * Adds a meta box to the post editing screen
+               */
+              function prfx_custom_meta() {
+                add_meta_box( 'prfx_meta', __( 'Vendor Information', 'prfx-textdomain' ), 'prfx_meta_callback', 'vendor' );
+              }
+              add_action( 'add_meta_boxes', 'prfx_custom_meta' );
+              /**
+               * Outputs the content of the meta box
+               */
+              function prfx_meta_callback( $post ) {
+                wp_nonce_field( basename( __FILE__ ), 'prfx_nonce' );
+                $prfx_stored_meta = get_post_meta( $post->ID );
+                ?>
+
+                <p>
+                  <label for="meta-text" class="prfx-row-title"><?php _e( 'Example Text Input', 'prfx-textdomain' )?></label>
+                  <input type="text" name="meta-text" id="meta-text" value="<?php if ( isset ( $prfx_stored_meta['meta-text'] ) ) echo $prfx_stored_meta['meta-text'][0]; ?>" />
+                </p>
+
+                <p>
+                  <span class="prfx-row-title"><?php _e( 'Example Checkbox Input', 'prfx-textdomain' )?></span>
+                  <div class="prfx-row-content">
+                    <label for="meta-checkbox">
+                      <input type="checkbox" name="meta-checkbox" id="meta-checkbox" value="yes" <?php if ( isset ( $prfx_stored_meta['meta-checkbox'] ) ) checked( $prfx_stored_meta['meta-checkbox'][0], 'yes' ); ?> />
+                      <?php _e( 'Checkbox label', 'prfx-textdomain' )?>
+                    </label>
+                    <label for="meta-checkbox-two">
+                      <input type="checkbox" name="meta-checkbox-two" id="meta-checkbox-two" value="yes" <?php if ( isset ( $prfx_stored_meta['meta-checkbox-two'] ) ) checked( $prfx_stored_meta['meta-checkbox-two'][0], 'yes' ); ?> />
+                      <?php _e( 'Another checkbox', 'prfx-textdomain' )?>
+                    </label>
+                  </div>
+                </p>
+
+                <p>
+                  <span class="prfx-row-title"><?php _e( 'Example Radio Buttons', 'prfx-textdomain' )?></span>
+                  <div class="prfx-row-content">
+                    <label for="meta-radio-one">
+                      <input type="radio" name="meta-radio" id="meta-radio-one" value="radio-one" <?php if ( isset ( $prfx_stored_meta['meta-radio'] ) ) checked( $prfx_stored_meta['meta-radio'][0], 'radio-one' ); ?>>
+                      <?php _e( 'Radio Option #1', 'prfx-textdomain' )?>
+                    </label>
+                    <label for="meta-radio-two">
+                      <input type="radio" name="meta-radio" id="meta-radio-two" value="radio-two" <?php if ( isset ( $prfx_stored_meta['meta-radio'] ) ) checked( $prfx_stored_meta['meta-radio'][0], 'radio-two' ); ?>>
+                      <?php _e( 'Radio Option #2', 'prfx-textdomain' )?>
+                    </label>
+                  </div>
+                </p>
+
+                <p>
+                  <label for="meta-select" class="prfx-row-title"><?php _e( 'Example Select Input', 'prfx-textdomain' )?></label>
+                  <select name="meta-select" id="meta-select">
+                    <option value="select-one" <?php if ( isset ( $prfx_stored_meta['meta-select'] ) ) selected( $prfx_stored_meta['meta-select'][0], 'select-one' ); ?>><?php _e( 'One', 'prfx-textdomain' )?></option>';
+                    <option value="select-two" <?php if ( isset ( $prfx_stored_meta['meta-select'] ) ) selected( $prfx_stored_meta['meta-select'][0], 'select-two' ); ?>><?php _e( 'Two', 'prfx-textdomain' )?></option>';
+                  </select>
+                </p>
+
+                <p>
+                  <label for="meta-textarea" class="prfx-row-title"><?php _e( 'Example Textarea Input', 'prfx-textdomain' )?></label>
+                  <textarea name="meta-textarea" id="meta-textarea"><?php if ( isset ( $prfx_stored_meta['meta-textarea'] ) ) echo $prfx_stored_meta['meta-textarea'][0]; ?></textarea>
+                </p>
+
+                <p>
+                  <label for="meta-color" class="prfx-row-title"><?php _e( 'Color Picker', 'prfx-textdomain' )?></label>
+                  <input name="meta-color" type="text" value="<?php if ( isset ( $prfx_stored_meta['meta-color'] ) ) echo $prfx_stored_meta['meta-color'][0]; ?>" class="meta-color" />
+                </p>
+
+                <p>
+                  <label for="meta-image" class="prfx-row-title"><?php _e( 'Example File Upload', 'prfx-textdomain' )?></label>
+                  <input type="button" id="meta-image-button" class="button" value="<?php _e( 'Choose or Upload an Image', 'prfx-textdomain' )?>" />
+                </p>
+
+                <p>
+                  <img name="meta-image" id="meta-image" src="<?php if ( isset ( $prfx_stored_meta['meta-image'] ) ) echo $prfx_stored_meta['meta-image'][0]; ?>" alt="" />
+                </p>
+               
+
+                <?php
+              }
+              /**
+               * Saves the custom meta input
+               */
+              function prfx_meta_save( $post_id ) {
+               
+                // Checks save status
+                $is_autosave = wp_is_post_autosave( $post_id );
+                $is_revision = wp_is_post_revision( $post_id );
+                $is_valid_nonce = ( isset( $_POST[ 'prfx_nonce' ] ) && wp_verify_nonce( $_POST[ 'prfx_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+               
+                // Exits script depending on save status
+                if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+                  return;
+                }
+               
+                // Checks for input and sanitizes/saves if needed
+                if( isset( $_POST[ 'meta-text' ] ) ) {
+                  update_post_meta( $post_id, 'meta-text', sanitize_text_field( $_POST[ 'meta-text' ] ) );
+                }
+                // Checks for input and saves
+                if( isset( $_POST[ 'meta-checkbox' ] ) ) {
+                  update_post_meta( $post_id, 'meta-checkbox', 'yes' );
+                } else {
+                  update_post_meta( $post_id, 'meta-checkbox', '' );
+                }
+                 
+                // Checks for input and saves
+                if( isset( $_POST[ 'meta-checkbox-two' ] ) ) {
+                  update_post_meta( $post_id, 'meta-checkbox-two', 'yes' );
+                } else {
+                  update_post_meta( $post_id, 'meta-checkbox-two', '' );
+                }
+                // Checks for input and saves if needed
+                if( isset( $_POST[ 'meta-radio' ] ) ) {
+                  update_post_meta( $post_id, 'meta-radio', $_POST[ 'meta-radio' ] );
+                }
+                // Checks for input and saves if needed
+                if( isset( $_POST[ 'meta-select' ] ) ) {
+                  update_post_meta( $post_id, 'meta-select', $_POST[ 'meta-select' ] );
+                }
+                // Checks for input and saves if needed
+                if( isset( $_POST[ 'meta-textarea' ] ) ) {
+                  update_post_meta( $post_id, 'meta-textarea', $_POST[ 'meta-textarea' ] );
+                }
+                // Checks for input and saves if needed
+                if( isset( $_POST[ 'meta-color' ] ) ) {
+                  update_post_meta( $post_id, 'meta-color', $_POST[ 'meta-color' ] );
+                }
+                // Checks for input and saves if needed
+                if( isset( $_POST[ 'meta-image' ] ) ) {
+                  update_post_meta( $post_id, 'meta-image', $_POST[ 'meta-image' ] );
+                }
+              }
+              add_action( 'save_post', 'prfx_meta_save' );
+              /**
+               * Adds the meta box stylesheet when appropriate
+               */
+              function prfx_admin_styles(){
+                global $typenow;
+                if( $typenow == 'vendor' ) {
+                  wp_enqueue_style( 'prfx_meta_box_styles', plugin_dir_url( __FILE__ ) . 'meta-box-styles.css' );
+                }
+              }
+              add_action( 'admin_print_styles', 'prfx_admin_styles' );
+              /**
+               * Loads the color picker javascript
+               */
+              function prfx_color_enqueue() {
+                global $typenow;
+                if( $typenow == 'vendor' ) {
+                  wp_enqueue_style( 'wp-color-picker' );
+                  wp_enqueue_script( 'meta-box-color-js', plugin_dir_url( __FILE__ ) . 'meta-box-color.js', array( 'wp-color-picker' ) );
+                }
+              }
+              add_action( 'admin_enqueue_scripts', 'prfx_color_enqueue' );
+              /**
+               * Loads the image management javascript
+               */
+              function prfx_image_enqueue() {
+                global $typenow;
+                if( $typenow == 'vendor' ) {
+                  wp_enqueue_media();
+               
+                  // Registers and enqueues the required javascript.
+                  wp_register_script( 'meta-box-image', plugin_dir_url( __FILE__ ) . 'meta-box-image.js', array( 'jquery' ) );
+                  wp_localize_script( 'meta-box-image', 'meta_image',
+                    array(
+                      'title' => __( 'Choose or Upload an Image', 'prfx-textdomain' ),
+                      'button' => __( 'Use this image', 'prfx-textdomain' ),
+                    )
+                  );
+                  wp_enqueue_script( 'meta-box-image' );
+                }
+              }
+              add_action( 'admin_enqueue_scripts', 'prfx_image_enqueue' );
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- theme/single.php
+++ theme/single.php
@@ -6,6 +6,21 @@
     <?php the_post(); ?>
     <?php the_content(); ?>
     <?php edit_post_link( __( 'Edit', 'TB2017' ), '<span class="edit-link">', '</span>' ) ?>
-    <?php echo get_post_meta(get_the_ID(), 'vendor_img_widget_1', true); ?>
+
+
+
+
+
+<?php
+ 
+    // Retrieves the stored value from the database
+    $meta_value = get_post_meta( get_the_ID(), 'meta-image', true );
+ 
+    // Checks and displays the retrieved value
+    if( !empty( $meta_value ) ) {
+        echo $meta_value;
+    }
+ 
+?>
 </div>
 <?php get_footer(); ?>
